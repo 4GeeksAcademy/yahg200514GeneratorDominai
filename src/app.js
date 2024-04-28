665512c3813140cd336da75296c4109867504837
Yahg2005 <161893815+Yahg2005@users.noreply.github.com>
2024-04-28 15:52:41 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,26 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+//console.log("Hola, mundo!");
+const pronouns = ["the", "our"];
+const adjs = ["great", "big"];
+const nouns = ["jogger", "racoon"];
+const extensions = [".com.es.net"];
+const generateDomainNames = (pronouns, adjs, nouns, extensions) => {
+    const domainNames = [];
+     for (const pronoun of pronouns) {
+        for (const adj of adjs) {
+            for (const noun of nouns) {
+            for (const extension of extensions) {
+                const domainName = `${pronoun}${adj}${noun}${extension}`;
+                domainNames.push(domainName);
+                }
+            }
+        }
+    }
+    return domainNames;
+};
+const domainNames = generateDomainNames(pronouns, adjs, nouns,extensions);
+for (const domainName of domainNames) {
+    console.log(domainName);
+}
