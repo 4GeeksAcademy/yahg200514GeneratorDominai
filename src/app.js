461d0fe76bc254cf1f7eac6ae32cd3e0b0927a04
Yahg2005 <161893815+Yahg2005@users.noreply.github.com>
2024-05-04 07:22:04 +0000

    Fixed windows.onload

--- src/app.js
+++ src/app.js
@@ -1,19 +1,8 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-console.log("Hola, mundo!");
+//console.log("Hola, mundo!");
 const pronouns = ["the", "our"];
 const adjs = ["great", "big"];
 const nouns = ["jogger", "racoon"];
-const extensions = [".com.es.net"];
+const extensions = [".com", ".es", ".net"];
 const generateDomainNames = (pronouns, adjs, nouns, extensions) => {
   const domainNames = [];
   for (const pronoun of pronouns) {
